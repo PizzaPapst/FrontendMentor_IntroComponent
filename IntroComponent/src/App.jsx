6bf9a774c0cc5118bad348cf9328903d45ccfa18
PizzaPapst <maik.bartels2@gmail.com>
2025-09-18 17:49:47 +0200

    Mobile Styling

--- IntroComponent/src/App.jsx
+++ IntroComponent/src/App.jsx
@@ -57,17 +57,30 @@ function App() {
           <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable</p>
         </div>
         <div className='right-part'>
-          <button>
+          <button className='trial-button'>
             <span>Try it free 7 days </span> 
             than $20/mo. thereafter
           </button>
           <form action={handleSubmit} novalidate="novalidate">
-            <input type='text' id='firstname' name='firstname' placeholder='First Name' onChange={handleChange}/>
-            {errors.firstname && <span style={{color: "black"}}>{errors.firstname}</span>} 
-            <input type='text' id='lastname' name='lastname' placeholder='Last Name' onChange={handleChange}/>
-            {errors.lastname && <span style={{color: "black"}}>{errors.lastname}</span>} 
-            <input type='email' id='email' name='email' placeholder='Email Adress'/>
-            <input type='password' id='password' name='password' placeholder='Password' />
+            <div className='lvg'>
+              <input type='text' id='firstname' name='firstname' placeholder='First Name' onChange={handleChange}/>
+              {errors.firstname && <span>{errors.firstname}</span>} 
+            </div>
+
+            <div className='lvg'>
+              <input type='text' id='lastname' name='lastname' placeholder='Last Name' onChange={handleChange}/>
+              {errors.lastname && <span>{errors.lastname}</span>}
+            </div>
+
+            <div className='lvg'>
+              <input type='email' id='email' name='email' placeholder='Email Adress' onChange={handleChange}/>
+              {errors.email && <span>{errors.email}</span>} 
+            </div>
+
+            <div className='lvg'>
+              <input type='password' id='password' name='password' placeholder='Password' onChange={handleChange}/>
+              {errors.password && <span>{errors.password}</span>} 
+            </div>
             <button>CLAIM YOUR FREE TRIAL</button>
             <p>By clicking the Button you are agreeing our <a href=''>Terms and Services</a></p>
           </form>
